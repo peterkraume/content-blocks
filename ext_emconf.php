e057c1619adaa1fa481d7a7cf952f65a29bc7821
Peter Kraume <peter.kraume@gmx.de>
2024-03-03 19:08:18 +0100

    [TASK] Add compatibility with TYPO3 v13.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.6.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '12.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
